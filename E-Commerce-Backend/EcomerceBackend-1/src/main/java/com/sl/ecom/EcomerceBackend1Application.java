7a115748687ee868acac1430243aae7ed42667f3
SagarLekkalar0409 <sagarlekkalwar0409@gmail.com>
2025-09-30 09:50:27 +0530

    Initial commit with frontend and backend folders

---
+++ E-Commerce-Backend/EcomerceBackend-1/src/main/java/com/sl/ecom/EcomerceBackend1Application.java
@@ -0,0 +1,13 @@
+package com.sl.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceBackend1Application.class, args);
+	}
+
+}
---
+++ E-Commerce-Backend/EcomerceBackend-1/src/test/java/com/sl/ecom/EcomerceBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
